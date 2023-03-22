b0913784deb1d55df8d1638725bd8c4d58336e64
lightcraft <lightcraft@lightcrafts-MacBook-Pro.local>
2023-03-22 14:15:27 +0800

    create notes to dynamodb

--- handler.js
+++ handler.js
@@ -1,8 +1,54 @@
 'use strict';
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+const documentClient = new DynamoDB.DocumentClient({ region: 'us-east-1' });
 
-module.exports.createNote = async (event) => {
+const send = (statusCode, data) => {
+  return {
+    statusCode,
+    body: JSON.stringify(data),
+  };
+};
+
+module.exports.createNote = async (event, context, cb) => {
+  const data = JSON.parse(event.body);
+  console.log(data);
+  try {
+    const params = {
+      TableName: 'notes',
+      Item: {
+        notesId: data.id,
+        title: data.title,
+        body: data.body,
+      },
+      ConditionExpression: 'attribute_not_exists(notesId)',
+    };
+    await documentClient.put(params).promise();
+    cb(null, send(201, data));
+  } catch (err) {
+    console.log(err.message);
+    cb(null, send(500, err.message));
+  }
+};
+
+module.exports.updateNote = async (event) => {
+  const notesId = event.pathParameters.id;
   return {
     statusCode: 200,
-    body: JSON.stringify(`A new note created`)
-  }
+    body: JSON.stringify(`The note with id ${notesId} is updated`),
+  };
+};
+
+module.exports.deleteNote = async (event) => {
+  const notesId = event.pathParameters.id;
+  return {
+    statusCode: 200,
+    body: JSON.stringify(`The note with id ${notesId} is deleted`),
+  };
+};
+
+module.exports.getallNotes = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(`All notes retrieved`),
+  };
 };
